7163adbe346bc0c2d3d58c2f7eb812891e8e2aae
n3rfy <nerfy.11@mail.ru>
2021-09-10 16:51:20 +0300

    lab 1 add comments

--- lab1/Palindrome.java
+++ lab1/Palindrome.java
@@ -1,11 +1,13 @@
 public class Palindrome{
     public static void main(String [] args){
+        ///the main function starts a cycle all words
         for (int i = 0; i < args.length; i++){
             String s = args[i];
             System.out.println(isPalindrome(s));
         }
     }
     public static String reverseString(String s){
+        ///returns reverse words 
         String new_string = "";
         int len_string = s.length();
         for (int i = 1; i <= len_string; i++){
@@ -15,6 +17,7 @@ public class Palindrome{
         return new_string;
     }
     public static boolean isPalindrome(String s){
+        ///returns whether the word is Palindrome
         String new_string = reverseString(s);
         return s.equals(new_string);
     }
--- lab1/Primes.java
+++ lab1/Primes.java
@@ -1,5 +1,6 @@
 public class Primes {
     public static void main(String[] args) {
+        ///the main function starts a cycle of numbers from 2 to 100
         for (int i=2; i<100; i++)
             if(isPrime(i))
                 System.out.println(i);
@@ -7,6 +8,7 @@ public class Primes {
 
 
     public static boolean isPrime(int n){
+        ///checks a number for primes
         for (int i=2; i<n; i++)
             if(n%i ==0)
                 return false;
